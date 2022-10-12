b5365baf1a5e243d8313392683b948b2f767387d
aubepluieh3 <aubepluieh3@naver.com>
2022-10-12 20:39:26 +0900

    #1.0 Get Selectors

---
+++ src/theme.tsx
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "#464548",
+  accentColor: "#ead5ff",
+  cardBgColor: "transparent",
+};
