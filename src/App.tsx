0112dff214858ad8ad493405b4ccf9cb4235a47e
aubepluieh3 <aubepluieh3@naver.com>
2022-10-13 11:03:57 +0900

    #2.8 Cross Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ function App() {
   const onDragEnd = (info: DropResult) => {
     console.log(info);
     const { destination, draggableId, source } = info;
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       //same board movement.
       setToDos((allBoards) => {
@@ -36,7 +37,21 @@ function App() {
         boardCopy.splice(destination?.index, 0, draggableId);
         return {
           ...allBoards,
-          [(source.droppableId)]: boardCopy,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination.droppableId !== source.droppableId) {
+      // cross board movement
+      setToDos((allBoards) => {
+        const sourceBoarad = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoarad.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoarad,
+          [destination.droppableId]: destinationBoard,
         };
       });
     }
